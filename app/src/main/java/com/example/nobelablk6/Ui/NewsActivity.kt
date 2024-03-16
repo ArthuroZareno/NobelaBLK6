b2353e8d615b302a9dfe3b618f7ab73f03356915
iNathz <119001491+iNathz@users.noreply.github.com>
2024-03-17 02:31:06 +0800

    Pre requisites & XML Design

--- app/src/main/java/com/example/nobelablk6/Ui/NewsActivity.kt
+++ app/src/main/java/com/example/nobelablk6/Ui/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.example.nobelablk6
+package com.example.nobelablk6.Ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nobelablk6.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
